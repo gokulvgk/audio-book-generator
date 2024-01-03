9409087f3dd328ce3e22430d669da7f248c262a5
gokul krishnan <vgk04892882@gmail.com>
2024-01-03 20:31:59 +0530

    my module created
    created a module that includes all the other modules,functions and variables used

--- generator.py
+++ generator.py
@@ -1,102 +1,23 @@
-import os
-from gtts import gTTS
-from translate import Translator
-from tkinter import * 
-from tkinter import filedialog
-import subprocess
+import mymodule
 
-root =Tk()
-root.title("Text-to-speech conversion with translation")
-root.geometry("1000x800")
-root.iconbitmap("speaker-image.ico")
-languages=['']*6
-def open_dialog():
-    root.inputfilename =  filedialog.askopenfilename(initialdir = "/",title = "Select file",filetypes = (("text files","*.txt"),("all files","*.*")))
-    file_path_label = Label(root, text=root.inputfilename).pack()
-def open_audio_files():
-    root.outputfilename =  filedialog.askopenfilename(initialdir="audio_files", title="audio files for your text", filetypes=(("mp3 files", "*.mp3"),))
-    file_path_label = Label(root, text=root.outputfilename).pack()
-    subprocess.Popen(["start", " ", root.outputfilename], shell=True) 
-# Function to translate text to a target language
-def translate_text(text, target_language):
-    translator = Translator(to_lang=target_language)
-    translation = translator.translate(text)
-    return translation
-def text_to_speech(text, language, output_file):
-    if language == 'auto':
-        # Automatically detect the language
-        tts = gTTS(text, lang='auto')
-    else:
-        tts = gTTS(text, lang=language)
-
-    tts.save(output_file)
-def convert():
-    infolabel = Label(root, text="your audio files are ready")
-    infolabel.pack()
- # Input file containing the text you want to convert to speech
-    input_file = root.inputfilename
-# Output directory for audio files
-    output_directory = 'audio_files'
-    if not os.path.exists(output_directory):
-        os.makedirs(output_directory)
-# Loop through each language and convert the text to speech
-    with open(input_file, 'r') as file:
-        text = file.read()
-        for lang in languages:
-            translated_text = translate_text(text, lang)
-            output_file = os.path.join(output_directory, f'output_{lang}.mp3')
-            text_to_speech(translated_text, lang, output_file)
-            # text_to_speech_pyttsx3(translated_text, lang)
-               
-prompt_label = Label(root, text="Convert any text file into an audio file")
-prompt_label.pack()
-browsebutton = Button(root, text="browse files", command=open_dialog, fg="blue", bg="silver")
-browsebutton.pack(padx=20, pady=20)
-takes_time = Label(root, text="converting may take some time")
-takes_time.pack(padx=20, pady=20)
-
-# Function to check which checkbox is selected
-def check():
-    if en_var.get():
-        enselected = Label(root, text="English selected")
-        languages[0]='en'
-    if fr_var.get():
-        frselected = Label(root, text="French selected")
-        languages[1]='fr'
-    if nl_var.get():
-        nlselected = Label(root, text="Dutch selected")
-        languages[2]='nl'
-    if es_var.get():
-        esselected = Label(root, text="Spanish selected")
-        languages[3]='es'
-    if de_var.get():
-        deselected = Label(root, text="German selected")
-        languages[4]='de'
-    if ta_var.get():
-        taselected = Label(root, text="Tamil selected")
-        languages[5]='ta'
-# initializing tkinter variables to keep track of which checkbox is selected
-en_var= IntVar() 
-fr_var= IntVar()
-nl_var= IntVar()
-es_var= IntVar()
-de_var= IntVar()
-ta_var= IntVar()        
-checkbox1=Checkbutton(root, text="English", variable=en_var, command=check)
+mymodule.root.title("Text-to-speech conversion with translation")
+mymodule.root.geometry("1000x800")
+mymodule.root.iconbitmap("speaker-image.ico")     
+checkbox1=mymodule.Checkbutton(mymodule.root, text="English", variable=mymodule.en_var, command=mymodule.check)
 checkbox1.pack(padx=20, pady=20)
-checkbox2=Checkbutton(root, text="French", variable=fr_var, command=check)
+checkbox2=mymodule.Checkbutton(mymodule.root, text="French", variable=mymodule.fr_var, command=mymodule.check)
 checkbox2.pack(padx=20, pady=20)
-checkbox3=Checkbutton(root, text="Dutch", variable=nl_var, command=check)
+checkbox3=mymodule.Checkbutton(mymodule.root, text="Dutch", variable=mymodule.nl_var, command=mymodule.check)
 checkbox3.pack(padx=20, pady=20)
-checkbox4=Checkbutton(root, text="Spanish", variable=es_var, command=check)
+checkbox4=mymodule.Checkbutton(mymodule.root, text="Spanish", variable=mymodule.es_var, command=mymodule.check)
 checkbox4.pack(padx=20, pady=20)
-checkbox5=Checkbutton(root, text="German", variable=de_var, command=check)
+checkbox5=mymodule.Checkbutton(mymodule.root, text="German", variable=mymodule.de_var, command=mymodule.check)
 checkbox5.pack(padx=20, pady=20)
-checkbox6=Checkbutton(root, text="Tamil", variable=ta_var, command=check)
+checkbox6=mymodule.Checkbutton(mymodule.root, text="Tamil", variable=mymodule.ta_var, command=mymodule.check)
 checkbox6.pack(padx=20, pady=20)
-convertbutton= Button(root, text="convert",command=convert)
+convertbutton= mymodule.Button(mymodule.root, text="convert",command=mymodule.convert)
 convertbutton.pack(padx=20)
-view_audio_button = Button(root, text="View your Audio files", command=open_audio_files)
+view_audio_button = mymodule.Button(mymodule.root, text="View your Audio files", command=mymodule.open_audio_files)
 view_audio_button.pack(padx=20, pady=20)
-root.mainloop()
+mymodule.root.mainloop()
 print("Text-to-speech conversion with translation complete.")
